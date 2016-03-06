a63655708b4c6a18f3c3cb9cbc09a90135ee19b4
Dmitriy Muzyka <a5b1ca@gmail.com>
2016-03-06 18:53:25 +0300

    main

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import threading
 import json
 import time
 #from skype import SkypeChannel
 from slack import Slack
+
 with open('config.json') as f:
     settings = json.load(f)
     USERTOKENSTRING = settings['slack']['USERTOKENSTRING']
     CHANNEL_ID = settings['slack']['CHANNEL_ID']
     slack_oldest = settings['slack']['oldest']
     SkypeChatId = settings['skype']['ChatId']
-    delay = 1.0
 
 def writeSettings():
     settings['slack']['oldest'] = slack_oldest
@@ -33,6 +32,7 @@ def onMsgReceive(msg):
 
 slack = Slack(settings['slack']['USERTOKENSTRING'], settings['slack']['CHANNEL_ID'], True)
 slack.onMsgReceive = onMsgReceive
+
 while True:
     raw_input()
     time.sleep(1.0)
